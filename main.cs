8d84013a63f0416eccabcafb594f309a0a272dc5
Frant24 <36748891+Frant24@users.noreply.github.com>
2023-09-21 02:27:31 +0000

    Main upload

--- main.cs
+++ main.cs
@@ -1,7 +1,35 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the wind speed in mph:");
+        double windSpeed = double.Parse(Console.ReadLine());
+
+        if (windSpeed >= 157)
+        {
+            Console.WriteLine("Category Five Hurricane");
+        }
+        else if (windSpeed >= 130)
+        {
+            Console.WriteLine("Category Four Hurricane");
+        }
+        else if (windSpeed >= 111)
+        {
+            Console.WriteLine("Category Three Hurricane");
+        }
+        else if (windSpeed >= 96)
+        {
+            Console.WriteLine("Category Two Hurricane");
+        }
+        else if (windSpeed >= 74)
+        {
+            Console.WriteLine("Category One Hurricane");
+        }
+        else
+        {
+            Console.WriteLine("Not a Hurricane");
+        }
+    }
 }
